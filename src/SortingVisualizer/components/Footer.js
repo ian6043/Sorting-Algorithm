a9c4cba72879a1775a4e815df968587215561832
Ian Wilkinson <ianw6043@gmail.com>
2021-10-17 16:19:34 -0400

    fixed padding issue

--- src/SortingVisualizer/components/Footer.js
+++ src/SortingVisualizer/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="contact-header">Contact Me</div>
             <a href="mailto:ianw6043@gmail.com" className="social-link">ianw6043@gmail.com  </a>
             <a href="https://www.linkedin.com/in/ian-wilkinson-258499208" className="social-link">Linkedin</a>
-            <a href="https://github.com/ian6043" class="social-link">Github</a>
+            <a href="https://github.com/ian6043" className="social-link">Github</a>
         </div>
     )
 }
